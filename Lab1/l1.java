64d20651e890ef2247b7f32a85149511d8559110
Chinmaya <cinshu39@gmail.com>
2024-03-25 20:05:11 +0530

    Command Line

---
+++ Lab1/l1.java
@@ -0,0 +1,7 @@
+class Test{
+    public static void main(String[] x) {
+        System.out.println(Integer.parseInt(x[0]));
+        System.out.println(Integer.parseInt(x[1]));
+        System.out.println(Integer.parseInt(x[0])+Integer.parseInt(x[1]));
+    }
+}
